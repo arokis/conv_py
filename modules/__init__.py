c099cce36fe7560e3a2d330417d9e4080a1d43a6
Uwe Sikora <usikora@sub.uni-goettingen.de>
2017-02-13 14:53:22 +0100

    convPY's modules living here, inkl. classes

---
+++ modules/__init__.py
@@ -0,0 +1,1 @@
+from classes import Conversion, Xslt, Xquery
---
+++ modules/classes.py
@@ -0,0 +1,42 @@
+import subprocess, os, json
+
+################## Conversion Classes #########################
+
+class Conversion (object):
+    def __init__(self, obj, source):
+        #self.script = obj['script']
+        self.script = os.path.abspath(obj['script'])
+        self.language = obj['language']
+        self.engine = os.path.abspath(obj['engine'])
+        self.name = obj['name']
+        self.source = os.path.abspath(source)
+    
+    def speak (self):
+        print('I am the ' + self.name + '!')
+        print ('Conversion "' + self.name + '" via ' + self.language + ':' + str(self.engine) + ' using ' + self.script + ' on ' + self.source)
+
+    def createXML (self, content):
+        file = open(self.source,"w+") 
+        file.write(content) 
+        file.close()
+    
+    def call (self):
+        return " ".join((self.language, self.script, self.source))
+
+    def run (self):
+        output = subprocess.check_output(self.call(), shell=True)
+        self.createXML(output)
+
+#************ XSLT ***********
+class Xslt (Conversion):
+    def call (self):
+        script = "-xsl:" + self.script
+        source = "-s:" + self.source
+        return " ".join((self.language, self.engine, script, source))
+
+#************ XQUERY *********
+class Xquery (Conversion):
+    def call (self):
+        script = "-xquery:" + self.script
+        source = "-s:" + self.source
+        return " ".join((self.language, self.engine, script, source))
