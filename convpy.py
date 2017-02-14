9c868236d63df605ab7907d862c8277c5999c0f8
Uwe Sikora <usikora@sub.uni-goettingen.de>
2017-02-14 15:56:01 +0100

    structured workflow

--- convpy.py
+++ convpy.py
@@ -1,55 +1,132 @@
-import subprocess, os, json
-from modules import Xslt, Conversion, Xquery
+import json, os, sys, urllib2
+import modules
+
+# conv.py is living here
+convpy_home = os.path.dirname(os.path.abspath( __file__ ))
+
+# set default scenarios as given in scenarios.json
+default_scenarios = modules.scenarios
+
+# set default engines as given in config.json (default: xslt, xquery, other)
+default_engines = modules.engines
+
+tmpXML = modules.tmpXML
+
+#++++++++++++++ functions (import-ready) +++++++++++++
 
 
-##################### Functions #############################
 
-def configure (path):
-    path = os.path.abspath(path)
-    if os.path.isfile(path):
-        return parseJSONFile(path)
+def workflow (convflow):
+    result = []    
+    for step in convflow['steps']:
+        if step.get('scenario'):
+            default_scenarios[step['scenario']]['name'] = step['scenario']
+            result.append(default_scenarios[step['scenario']])
+        else:
+            result.append(step)
+    return result
 
-def parseJSONFile (file):
-    #file = file.encode('utf-8')
-    with open(file, 'r') as data:
-        return json.load(data)
+def eval (scenario):
+    scenario_type = scenario['type']
+    scenario['script'] = os.path.abspath(scenario['script'])
+    conversion = {}
+    #print scenario
+    
+    if scenario['type'] in [key for key in default_engines] and not scenario.get('engine'):
+        #conversion = default_engines[scenario_type]
+        scenario['engine'] = os.path.abspath(default_engines[scenario_type]['engine'])
+        scenario['language'] = default_engines[scenario_type]['language']
+    elif scenario.get('engine') and scenario.get('language'):
+        scenario['engine'] = os.path.abspath(scenario['engine'])
+    elif not scenario.get('engine') and scenario.get('language'):
+        scenario['engine'] = False
+    else:
+        #print('[CONVPY:ERROR] No engine or language defined on ' + scenario_script)
+        scenario = False
+    return scenario
 
+def convert (step, source):
+    if step['engine'] != False:
+        if step['type'] == 'xslt':
+            saxon = modules.Xslt(step, source)
+            saxon.run()
+            #print(saxon.call())
+            del saxon
+            
+    else:
+        conversion = modules.Conversion(step, source)
+        conversion.run()
+        #print(conversion.call())
+        del conversion
+        
 def outputFile (content, file):
     file = open(file,"w+") 
     file.write(content) 
     file.close()
-
-
-def workflow (convflow, scenarios):
-    result = []
-    scenario_index = [i for i in scenarios]
     
-    for step in convflow['steps']:
-        if step.get('scenario'):
-            scenarios[step['scenario']]['name'] = step['scenario']
-            result.append(scenarios[step['scenario']])
-        else:
-            result.append(step)
-    #print result
-    return result
 
-###################### Global Variables ######################
+def request (url):
+    response = urllib2.urlopen(url)
+    data = response.read()
+    return data
 
-# conv.py is living here
-home = os.path.dirname(os.path.abspath( __file__ ))
+def open_xml (f):
+    with open(f, 'r') as out:
+        output = out.read()
+    return output
 
-# load config JSON
-config_path = 'config/config.json'
-config = configure(config_path)
+def inform(xml, clean):
+    #print(os.path.dirname(xml))
+    #
+    with open(xml, 'r') as out:
+        output = out.read()
+    if clean == True:
+        dir = os.path.dirname(xml)
+        os.remove(xml)
+        os.rmdir(dir)
+    print output
+    #
 
-# load convPY's default scenarios JSON
-scenarios = configure(config['default-scenarios'])
+"""
+# not functional yet
+def readJson():
+    steps = sys.stdin.readlines()
+    return json.loads(steps[0])
+"""
 
-# load convPY's default convflow JSON
-convflow = configure(config['default-convflow'])
+################# MAIN Flow #########################
+def main ():
+    
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+    else:
+        url = 'http://coptot.manuscriptroom.com/community/vmr/api/transcript/get/?docID=690003&pageID=0-400&joinParts=true&format=teiraw'
 
-# set convPY's default engines
-engines = config['conversion']
+    
+    # Get the data 
+    #xml_data = request(url)
+    xml_data = open_xml('data/test_xml.xml')
 
-# set temp-file from config
-tmpXML = os.path.abspath(config["tmp-file"])
+    if not os.path.exists(os.path.dirname(tmpXML)):
+        os.makedirs(os.path.dirname(tmpXML))
+        modules.createTempFile(tmpXML, xml_data)
+
+    
+    
+    # sort the conversion steps
+    defined_convflow = modules.convflow
+    conversion = workflow(defined_convflow)
+   
+    
+    for scenario in conversion:
+        step = eval(scenario)     
+        #print step
+        convert(step, tmpXML)   
+    
+    # well ... fire output and remove tmp-data
+    inform(tmpXML, True)
+    
+
+if __name__ == '__main__':
+    main()
+
