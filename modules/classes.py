4465a67b0c516bb59ee8756429072e689e29afa5
uwe <arokis.u@gmail.com>
2017-02-15 01:41:56 +0100

    Subclasses Saxon and Script are on

--- modules/classes.py
+++ modules/classes.py
@@ -1,78 +1,64 @@
 import subprocess, os, json
 
-################## Conversion Classes #########################
-
+########### Main Conversion CLass ############
 class Conversion (object):
     def __init__(self, obj, source):
-        #self.script = obj['script']
-        self.script = os.path.abspath(obj['script'])
-        self.language = obj['language']
-        self.type = os.path.abspath(obj['type'])
-        self.engine = False
-        if obj['engine'] != False:
-            self.engine = os.path.abspath(obj['engine'])
-            
+        self.language = obj['conversion']['language']
         self.name = obj['name']
+        self.type = obj['type']
+        self.script = os.path.abspath(obj['script'])
         self.source = os.path.abspath(source)
     
+    def _process (self, call):
+        def createXML (source, content):
+            file = open(source,"w+") 
+            file.write(content) 
+            file.close()
+        output = subprocess.check_output(call, shell=True)
+        createXML(self.source, output)
+
+
+########### Subclass Script (Conversion CLass) ############
+class Script (Conversion):
+    def __init__(self, obj, source):
+        Conversion.__init__(self, obj, source)
+
     def speak (self):
-        print('I am the ' + self.name + '!')
-        print ('Conversion "' + self.name + '" via ' + self.language + ':' + str(self.engine) + ' using ' + self.script + ' on ' + self.source)
-
-    def createXML (self, content):
-        file = open(self.source,"w+") 
-        file.write(content) 
-        file.close()
+        print ('SCRIPT "' + self.name + '" (' + self.type + ') via ' + self.language + ' using ' + self.script + ' on ' + self.source)
     
-    def call (self):
-        return " ".join((self.language, self.script, self.source))
-
     def run (self):
-        output = subprocess.check_output(self.call(), shell=True)
-        self.createXML(output)
+        call = " ".join((self.language, self.script, self.source))
+        self._process(call)
 
-#************ XSLT ***********
-class Xslt (Conversion):
-    def call (self):
+
+########### Subclass Engine (Conversion CLass) ############
+class Engine (Conversion):
+    def __init__(self, obj, source):
+        Conversion.__init__(self, obj, source)
+        self.engine = obj['conversion']['engine']
+        self.path = os.path.abspath(obj['conversion']['path'])
+    
+    def speak (self):
+        print ('ENGINE: "' + self.name + '" (' + self.type + ') via ' + self.language + ' ' + self.path + ' using ' + self.script + ' on ' + self.source)
+
+
+########### Subclass Saxon (Engine CLass) ############
+class Saxon (Engine):
+    def __init__(self, obj, source):
+        Engine.__init__(self, obj, source)
+    
+    def speak (self):
+        print ('SAXON: "' + self.name + '" via ' + self.language + ' ' + self.path + ' using ' + self.script + ' on ' + self.source)
+    
+    def xslt (self):
         script = "-xsl:" + self.script
         source = "-s:" + self.source
-        return " ".join((self.language, self.engine, script, source))
+        call = " ".join((self.language, self.path, script, source))
+        self._process(call)
 
-#************ XQUERY *********
-class Xquery (Conversion):
-    def call (self):
-        script = "-xquery:" + self.script
+    def xquery (self):
+        script = "-xql:" + self.script
         source = "-s:" + self.source
-        return " ".join((self.language, self.engine, script, source))
-
-# Prototyping Saxon as Class
-#************ saxon *********
-class Saxon (object):
-    def __init__(self, obj, source):
-        #self.script = obj['script']
-        self.script = os.path.abspath(obj['script'])
-        self.type = obj['type']
-        self.language = obj['language']
-        self.type = os.path.abspath(obj['type'])
-        self.engine = False
-        if obj['engine'] != False:
-            self.engine = os.path.abspath(obj['engine'])
-            
-        self.name = obj['name']
-        self.source = os.path.abspath(source)
+        call = " ".join((self.language, self.path, script, source))
+        self._process(call)
     
-    def speak (self):
-        print('I am the ' + self.name + '!')
-        print ('Conversion "' + self.name + '" via ' + self.language + ':' + str(self.engine) + ' using ' + self.script + ' on ' + self.source)
-
-    def createXML (self, content):
-        file = open(self.source,"w+") 
-        file.write(content) 
-        file.close()
-    
-    def call (self):
-        return " ".join((self.language, self.script, self.source))
-
-    def run (self):
-        output = subprocess.check_output(self.call(), shell=True)
-        self.createXML(output)
