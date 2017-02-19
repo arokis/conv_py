9673744309e18ba1aa89cf04929f80d4c6de1535
uwe <arokis.u@gmail.com>
2017-02-19 23:35:26 +0100

    Conversion-Class handles scenarioise

--- Conversion.py
+++ Conversion.py
@@ -2,29 +2,44 @@
 # *************
 # Defines the Conversion Class for each Step in a Conversion-Scenario
 # IN WORK !
-#
+
+import os
+#import Config
+import functions
 
 class Conversion (object):
-    def __init__(self, name, type, script):
-        self.name = name
-        self.type = conv_type
-        self.desc = desc
-        self.script = os.path.abspath(script) 
+
+    def __init__(self):
+        self.name = False
+        self.type = False
+        self.desc = False
+        self.script = False 
         self.converter = False
-
+        
     def info (self):
-        print ('Converison "' + self.name + '" as ' + self.type + ' with ' + self.script + ' doing "' + self.desc + '" (Custom-Converter: ' + self.converter +')')
-
-    def callConverter (self):
-        if self.converter == False:
-            if self.type == 'xslt':
-                # Call Saxon.xslt()
-                pass
-            elif self.type == 'xquery':
-                # Call Saxon.xquery()
-                pass
-            else: 
-                print ('Type unknown')
+        print self.name
+        print self.type
+        print self.desc
+        print self.script
+        print self.converter
+    
+    
+    def scenarioise (self, conversion, scripts):
+        #self.path = os.path.dirname( scripts_config )
+        #print scripts_path
+        
+        if conversion.get('scenario'):
+            #print ('SCENARIO!')
+            self.name = conversion['scenario']
+            self.type = scripts[self.name]['type']
+            self.desc = scripts[self.name]['desc']
+            self.script = scripts[self.name]['script']
         else:
-            #Call Call.run()
-
+            #print ('USERDEFINED!')
+            self.name = conversion['name']
+            self.type = conversion['type']
+            self.desc = conversion['desc']
+            self.script = os.path.abspath(conversion['script'])
+            self.language = conversion['language']
+            self.converter = ' '.join ( (conversion['language'], self.script) )
+    
