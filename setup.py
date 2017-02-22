6bbb5e9137ef10f19c36e3cdb03ce5bf0ffae3aa
uwe <arokis.u@gmail.com>
2017-02-22 20:21:29 +0100

    created setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ConvPY',
+    version='0.0.1',
+    description='Conversion TOOL for XML-Data',
+    # url
+    author="Uwe Sikora",
+    author_email="arokis.u@gmail.com",
+    # license = 'MIT'
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5'
+    ],
+    # keywords
+    # packages=find_packages(exclude=[]),
+    install_requires=[],
+    # pip install -e .[demonstrator,vis,evaluation]
+    )
