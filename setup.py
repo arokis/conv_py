6c7bba69f4ff253a58a315793076d38ee2a54324
uwe <arokis.u@gmail.com>
2017-02-22 21:24:38 +0100

    minor version-changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ConvPY',
-    version='0.0.1',
+    version='0.8.1',
     description='Conversion TOOL for XML-Data',
     # url
     author="Uwe Sikora",
