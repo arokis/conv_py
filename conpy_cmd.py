3cbe92d6d6a4156d6da10d004dc624be9ec6dd87
uwe <arokis.u@gmail.com>
2017-02-24 19:44:14 +0100

    catch TypeError while reading file

--- conpy_cmd.py
+++ conpy_cmd.py
@@ -20,13 +20,6 @@ __email__ = "arokis.u@googlemail.com"
 __date__ = "2017-02-12"
 
 
-
-
-
-
-
-
-
 ################# MAIN #################
 def main ():
 
@@ -36,14 +29,19 @@ def main ():
     parser.add_argument('-wf', '--flow', help='runs convPY on defined workflow')
     parser.add_argument('-o', '--out', default='out/', help='defines output-directory')
     #parser.add_argument('-iter', action='store_true', default='False', help='iterate through directory')
-    args = parser.parse_args(['-wf', 'scenarios/vmr2cs-nlp.json'])
-    
+    #args = parser.parse_args(['-wf', 'scenarios/vmr2cs-nlp2.json', '-i', 'http://coptot.manuscriptroom.com/community/vmr/api/transcript/get/?docID=690003&pageID=0-400&joinParts=true&format=teiraw'])
+    args = parser.parse_args()
+
     config = args.config
     source = args.input
     
     # open scenario from file
-    with open(args.flow, 'r') as file:
-        flow = file.read()
+    try:
+        with open(args.flow, 'r') as file:
+            flow = file.read()
+    except TypeError:
+        print ('[convPY:ERROR] Could not identify workflow! Exit!')
+        sys.exit(1)
     
     # loads the json-data
     scenario = json.loads(flow)
