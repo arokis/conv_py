97c8ae9465293fd93d9803dc918a699819d06473
uwe <arokis.u@gmail.com>
2017-02-25 00:39:59 +0100

    init imports APIs

--- convpy/__init__.py
+++ convpy/__init__.py
@@ -5,7 +5,8 @@
 convpy/__init__.py
 ******************
 
-initialises the main convPY-classes and functions
+initialises the main convPY-classes, APIs and functions
 """
 
 from conv import ConvPY
+from apis import stdin_api, cmd_api
