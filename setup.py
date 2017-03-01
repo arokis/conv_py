b15fd029fdc55581ad55d988f7045c20cf97a465
arokis <arokis.u@gmail.com>
2017-03-01 17:04:25 +0100

    edited setuo.py and erased errors

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='ConvPY',
     version='0.8.1',
     description='Conversion TOOL for XML-Data using SAXON and custom workflows',
-    download_url = 'https://github.com/arokis/conv_py.git'
+    download_url = 'https://github.com/arokis/conv_py.git',
     author="Uwe Sikora",
     author_email="arokis.u@gmail.com",
     license = 'MIT',
@@ -13,9 +13,9 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5'
     ],
-    keywords = 'xslt xsl xml saxon conversion converter xquery'
-    # packages=find_packages(exclude=[]),
+    keywords = 'xslt xsl xml saxon conversion converter xquery',
+    packages=['convpy'],
     install_requires=[
         'python-magic>=0.4.12'
-    ],    
+    ]    
 )
